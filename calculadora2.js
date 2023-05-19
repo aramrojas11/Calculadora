d073bdac9768d62dc7fab5e14ffd386f9c636103
aramrojas11 <aramrojas11@gmail.com>
2023-05-18 19:18:10 -0600

    Calculadora2

---
+++ calculadora2.js
@@ -0,0 +1,168 @@
+
+var auxcall = 1
+
+function agregarnumero(numero){
+    var resultado = document.getElementById("resultado");
+    resultado.value += numero;
+}
+function agregaroperador(operador){
+    var resultado = document.getElementById("resultado");
+    resultado.value += resultado;
+}
+
+
+function siete(){
+
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 7
+    document.getElementById("c1").value=nfinal1
+  }
+    if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal2 = c1 + 7
+      document.getElementById("c1").value = nfinal2
+    }
+  }
+
+function ocho(){
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 8
+    document.getElementById("c1").value=nfinal1
+  }
+  if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal2 = c1 + 8
+      document.getElementById("c1").value = nfinal2
+    }
+}
+function nueve(){
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 9
+    document.getElementById("c1").value=nfinal1
+  }
+  if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal2 = c1 + 9
+      document.getElementById("c1").value = nfinal2
+    }
+}
+function cuatro(){
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 4
+    document.getElementById("c1").value=nfinal1
+  }
+  if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal2 = c1 + 7
+      document.getElementById("c1").value = nfinal2
+    }
+}
+function cinco(){
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 5
+    document.getElementById("c1").value=nfinal1
+  }
+  if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal2 = c1 + 5
+      document.getElementById("c1").value = nfinal2
+    }
+}
+function seis(){
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 6
+    document.getElementById("c1").value=nfinal1
+  }
+  if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal2 = c1 + 6
+      document.getElementById("c1").value = nfinal2
+    }
+}
+function uno(){
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 1
+    document.getElementById("c1").value=nfinal1
+  }
+  if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal2 = c1 + 1
+      document.getElementById("c1").value = nfinal2
+    }
+}
+function dos(){
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 2
+    document.getElementById("c1").value=nfinal1
+  }
+  if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal1 = c1 + 2
+      document.getElementById("c1").value = nfinal2
+    }
+}
+function tres(){
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 3
+    document.getElementById("c1").value=nfinal1
+  }
+  if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal2 = c1 + 3
+      document.getElementById("c1").value = nfinal2
+    }
+}
+function suma(){
+
+  }
+function cero(){
+  if(auxcall == 1){
+    c1 = document.getElementById("c1").value
+    nfinal1 = c1 + 0
+    document.getElementById("c1").value=nfinal1
+  }
+  if(auxcall == 2){
+      c1 = document.getElementById("c1").value
+      nfinal2 = c1 + 0
+      document.getElementById("c1").value = nfinal2
+    }
+
+}
+function resta(){
+
+}
+function multi(){
+
+}
+function div(){
+
+}
+function raiz(){
+
+}
+function potencia(){
+
+}
+function mem(){
+
+}
+function borrar(){
+
+}
+function resultado(){
+
+}
+function punto(){
+
+
+}
+
